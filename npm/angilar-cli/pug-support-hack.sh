798aa56276e4a67ba7fef6979e80b94b093920a7
Yury Tvardovsky <oziabr@gmail.com>
2017-06-01 19:54:25 +0300

    Create pug-support-hack.sh

---
+++ npm/angilar-cli/pug-support-hack.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sed -i "/let webpackConfigs =/a \/\/ pug support hack\n            { module:{rules: [{test: /\\\\.pug/,\
+loaders: ['raw-loader', 'pug-html-loader?plugins[]=pug-plugin-ng'] }]}}," node_modules/@angular/cli/models/webpack-config.js
