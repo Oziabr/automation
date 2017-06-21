d96d39cc9aaaf907de57926b6a93c05b63824bbc
Yury Tvardovsky <oziabr@gmail.com>
2017-06-21 08:32:32 +0300

    Update useradd.sh

--- debian/useradd.sh
+++ debian/useradd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 groupadd oziabr
-useradd oziabr -m -g oziabr -G sudo
+useradd oziabr -s /bin/bash -m -g oziabr -G sudo
 cp -r ~/.ssh/ /home/oziabr/
 chown -R oziabr:oziabr /home/oziabr/.ssh
