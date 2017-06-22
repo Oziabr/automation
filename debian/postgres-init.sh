519905fea14b2115f526e684f77bef89f858909f
Yury Tvardovsky <oziabr@gmail.com>
2017-06-22 16:48:04 +0300

    Update postgres-init.sh

--- debian/postgres-init.sh
+++ debian/postgres-init.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+PASS=`openssl rand -base64 32`
+
 echo '\pset pager off' > ~/.psqlrc
 echo "create user $USER with superuser; create database $USER;" | sudo -u postgres psql
-echo '\du' | psql
+echo "alter user $USER with password '$PASS';" | sudo -u postgres psql
+echo '\du' | psql -h 127.0.0.1
+
+echo "127.0.0.1:*:*:$USER:$PASS" > ~/.pgpass
+chmod 0600 ~/.pgpass
 
 # echo "drop database $USER; drop user $USER; \du" | sudo -u postgres psql
