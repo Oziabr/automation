8930d69d808fd592d34484c0b53fadf116d65576
Yury Tvardovsky <oziabr@gmail.com>
2021-03-01 13:13:43 +0300

    Update bash_git_branch.sh

--- debian/bash_git_branch.sh
+++ debian/bash_git_branch.sh
@@ -2,6 +2,6 @@
 cat << EOF >> ~/.bashrc
 
 # display git branch in bash prompt
-export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'`\[\033[00m\] $ "
+export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/')\[\033[00m\] $ "
 
 EOF
